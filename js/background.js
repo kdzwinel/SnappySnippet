20615c8e3615127d674482e491b2eac7538e706a
Konrad Dzwinel <kdzwinel@gmail.com>
2013-11-20 23:40:12 +0100

    Adding comments on how the current localStorage can be improved in the future.

--- js/background.js
+++ js/background.js
@@ -1,5 +1,8 @@
 /*
 Allows to read, change and override settings kept in localStorage
+
+FIXME Can be replaced with chrome.storage.local as soon as http://crbug.com/178618 will be resolved
+FIXME Can be replaced with localStorage on the panel page as soon as http://crbug.com/319328 will be resolved
  */
 chrome.runtime.onMessage.addListener(function(message, sender, callback){
 	if(message.name === 'getSettings') {
