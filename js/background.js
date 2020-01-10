197faab1dc57b230c5eed145546fe34363609262
Kartik Soneji <kartiksoneji@rocketmail.com>
2020-01-11 00:15:11 +0530

    Switched localstorage to chrome.storage.local as per FIXME
    Resolved as Bug is fixed: "FIXME Can be replaced with chrome.storage.local as soon as http://crbug.com/178618 will be resolved"

--- js/background.js
+++ js/background.js
@@ -1,17 +1,19 @@
 /*
  Allows to read, change and override settings kept in localStorage
-
- FIXME Can be replaced with chrome.storage.local as soon as http://crbug.com/178618 will be resolved
- FIXME Can be replaced with localStorage on the panel page as soon as http://crbug.com/319328 will be resolved
  */
-chrome.runtime.onMessage.addListener(function (message, sender, callback) {
+chrome.runtime.onMessage.addListener((message, sender, callback) => {
 	"use strict";
 
-	if (message.name === 'getSettings') {
+	if(message.name === 'getSettings')
 		callback(localStorage);
-	} else if (message.name === 'setSettings') {
-		localStorage = message.data;
-	} else if (message.name === 'changeSetting') {
-		localStorage[message.item] = message.value;
+	
+	else if(message.name === 'setSettings')
+		chrome.storage.local.set(message.data);
+	
+	else if(message.name === 'changeSetting') {
+		let data = {};
+		data[message.item] = message.value;
+		
+		chrome.storage.local.set(data);
 	}
 });
